da62c1f6a48c725c46f0f745a0ff71a91052673e
Su Jiyuan <jiyuan0125@126.com>
2023-01-01 22:02:15 +0800

    modify enum foreground

--- src/theme.js
+++ src/theme.js
@@ -234,7 +234,7 @@ function getTheme({ name }) {
       typeParameter: primer.cyan[7],
       lifetime: primer.cyan[7],
       namespace: primer.black,
-      enumMember: { foreground: primer.black, fontStyle: "italic" },
+      enumMember: { foreground: primer.blue[6], fontStyle: "italic" },
       "*.attribute": primer.yellow[8],
       "*.mutable": {
         fontStyle: "underline",
